a206c34cdffd7550bb486ac51989ccb66f54f51c
Miryam Boucard <mimibozark@gmail.com>
2026-05-28 11:44:40 -0400

    added redux store and configured gallery slice

---
+++ src/data/GallerySlice.jsx
@@ -0,0 +1,11 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const GallerySlice = createSlice({
+    name: 'gallery',
+    initialState:{
+        items: []
+    },
+    reducers: {
+    }
+})
+export default GallerySlice.reducer;
---
+++ src/data/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit';
+import GalleryReducer from './GallerySlice';
+
+const store = configureStore({
+    reducers: {
+
+    }
+});
+export default store;
